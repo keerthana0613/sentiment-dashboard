66a4dce1d133d9e82efb99a2334b021ecf2bf54a
keerthana0613 <keerthanaaa0613@gmail.com>
2026-03-23 10:23:30 +0530

    updated api url

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://sentiment-dashboard-f9wc.onrender.com";
 
 export const uploadCSV = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
 
   const response = await fetch(
-    "http://127.0.0.1:8000/api/upload-csv",
+    "https://sentiment-dashboard-f9wc.onrender.com/api/upload-csv",
     {
       method: "POST",
       body: formData,
